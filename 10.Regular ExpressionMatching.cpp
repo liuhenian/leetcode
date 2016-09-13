5617219fbc97cc628642f5df194ead2b1c4d4dfe
nian <liuhenian0420@gmail.com>
2016-09-13 19:03:50 +0800

    第10题
    Regular ExceptionMatching

---
+++ 10.Regular ExpressionMatching.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();
+        if (p.size() == 1) {
+            return (s.size() == 1 && (s[0] == p[0] || p[0] == '.'));
+        }
+        if (p[1] != '*') {
+            if (s.empty()) return false;
+            return (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        }
+        while (!s.empty() && (s[0] == p[0] || p[0] == '.')) {
+            if (isMatch(s, p.substr(2))) return true;
+            s = s.substr(1);
+        }
+        return isMatch(s, p.substr(2));
+    }
+
+
+
+int main()
+{
+    string a1 = "aaabb";
+    string b2 = "a*abb";
+    bool target = isMatch(a1, b2);
+    cout<< target<<endl;
+}
